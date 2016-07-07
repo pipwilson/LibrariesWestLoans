28b892233b72e1addfe892257f1982c890954884
Phil Wilson <github@philwilson.org>
2016-07-07 23:24:26 +0100

    Start to add tests to Catalogue class

---
+++ app/src/test/java/philwilson/org/librarieswest/CatalogueTest.java
@@ -0,0 +1,16 @@
+package philwilson.org.librarieswest;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * Created by Phil on 07/07/2016.
+ */
+public class CatalogueTest {
+
+    @Test
+    public void testLogin() throws Exception {
+
+    }
+}
